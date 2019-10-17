4d44fe741943995cf04a6161d71a197389596cd8
SelvinMedina <selvinmedina0016@gmail.com>
2019-10-17 00:34:56 -0600

    Subir archivos en NodeJS terminado

--- app.js
+++ app.js
@@ -13,6 +13,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json()); //Cualquier peticion la convierte en JSON
 
 // CORS
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
 
 // Rutas
 app.use('/api', projectRoutes);
--- controllers/project.js
+++ controllers/project.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var Project = require('../models/project');
+var fs = require('fs');
 
 var controller = {
     home: function (req, res) {
@@ -67,6 +68,31 @@ var controller = {
                 project: projectRemoved
             });
         });
+    },
+    uploadImage: function (req, res) {
+        var projectId = req.params.id;
+        var errorImagen = 'Imagen no subida..';
+        if (req.files) {
+            var filePath = req.files.image.path;
+            var fileSplit = filePath.split('\\')
+            var fileName = fileSplit[1];
+            var extSplit = fileName.split('\.');
+            var fileExt = extSplit[1];
+            if (fileExt == 'png' || fileExt == 'jpg' || fileExt == 'jpeg' || fileExt == 'gif') {
+                Project.findByIdAndUpdate(projectId, { image: fileName }, { new: true }, (err, projectUpdated) => {
+                    if (err) return res.status(500).send({ message: 'La imagen no se ha subido' });
+                    if (!projectUpdated) return res.status(404).send({ message: 'El proyecto no existe' });
+                    return res.status(200).send({ project: projectUpdated });
+                });
+            }
+            else {
+                fs.unlink(filePath, (err) => {
+                    return res.status(200).send({ message: 'La extension no es valida' });
+                });
+            }
+        } else {
+            return res.status(200).send({ message: errorImagen });
+        }
     }
 };
 
