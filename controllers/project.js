0202b4fc30fdc7db623fc11f1e18d914d44dbdcd
SelvinMedina <selvinmedina0016@gmail.com>
2019-10-18 23:01:48 -0600

    Enviar imagenes, listo

--- controllers/project.js
+++ controllers/project.js
@@ -2,7 +2,7 @@
 
 var Project = require('../models/project');
 var fs = require('fs');
-
+var path = require('path');
 var controller = {
     home: function (req, res) {
         return res.status(200).send({
@@ -93,6 +93,20 @@ var controller = {
         } else {
             return res.status(200).send({ message: errorImagen });
         }
+    },
+    getImageFile: function (req, res) {
+        var file = req.params.image;
+        var path_file = './uploads/' + file;
+
+        fs.exists(path_file, (exists)=>{
+            if(exists){
+                return res.sendFile(path.resolve(path_file));
+            }else{
+                return res.status(404).send({
+                    message: 'No existe la imagen'
+                });
+            }
+        });
     }
 };
 
