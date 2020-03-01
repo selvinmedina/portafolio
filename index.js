2844c5950b2b6d8ac29fe19221c763ee184719e6
selvinmedina <selvinmedina0016@gmail.com>
2020-03-01 13:29:51 -0600

    cadena de conexion

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var app = require('./app');
 var port = 3700;
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/portafolio')
+mongoose.connect('mongodb+srv://selvin:1234@selvin-34yz6.mongodb.net/test?retryWrites=true&w=majority')
     .then(() => {
         console.log('Conexion a la base de datos establecida con satisfactoriamente!');
         // Creacion del servidor
